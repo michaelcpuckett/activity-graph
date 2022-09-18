f089f1f6cbbbad201bcd08afba6353c9f58b4d2e
Michael Puckett <michaelcpuckett@gmail.com>
2022-09-18 12:18:41 -0400

    start graph

---
+++ lib/graph/index.ts
@@ -0,0 +1,17 @@
+import { Db, MongoClient } from 'mongodb';
+
+export class Graph {
+  db: Db;
+
+  constructor(db: Db) {
+    this.db = db;
+  }
+
+  static async connect() {
+    const dbName = 'activitypub';
+    const client = new MongoClient("mongodb://testing:testing@localhost:27017");
+    await client.connect();
+    const db = client.db(dbName);
+    return new Graph(db);
+  }
+}

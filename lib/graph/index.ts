fd46631ed7903cf7e55c1a38a764a7f550150655
Michael Puckett <michaelcpuckett@gmail.com>
2022-09-23 18:03:45 -0400

    dont expand public actor

--- lib/graph/index.ts
+++ lib/graph/index.ts
@@ -275,17 +275,24 @@ export class Graph {
       if (key === 'id' || key === 'url' || key === 'type' || key === CONTEXT || key === '_id' || key === 'publicKey') {
         expanded.push([key, value]);
       } else if (typeof value === 'string') {
-        try {
-          const url = new URL(value);
-          expanded.push([key, await this.queryById(url.toString())]);
-        } catch (error) {
+        if (value === PUBLIC_ACTOR) {
           expanded.push([key, value]);
+        } else {
+          try {
+            const url = new URL(value);
+            expanded.push([key, await this.queryById(url.toString())]);
+          } catch (error) {
+            expanded.push([key, value]);
+          }
         }
       } else if (Array.isArray(value)) {
         const array = [...value];
         if (array.every((item: unknown) => typeof item === 'string')) {
           expanded.push([key, await Promise.all(array.map(async item => {
             if (typeof item === 'string') {
+              if (item === PUBLIC_ACTOR) {
+                return item;
+              }
               try {
                 const url = new URL(item);
                 return await this.queryById(url.toString());

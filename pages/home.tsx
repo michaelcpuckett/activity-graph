34c456139e0416052ae4f3ffc2d9ccbfb132d298
Michael Puckett <michael@pickpuck.onmicrosoft.com>
2022-10-05 15:50:24 -0400

    handle new format for home

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,7 @@
 import { HomePage } from "../components/HomePage";
 import { getServerSideProps as getHomeServerSideProps } from "activitypub-core/src/endpoints/home";
 import serviceAccount from "../credentials";
+import { NextPageContext } from "next";
 
-export const getServerSideProps = getHomeServerSideProps(serviceAccount);
+export const getServerSideProps = async (context: NextPageContext) => await getHomeServerSideProps(context, serviceAccount);
 export default HomePage;
